c1cc374fe6354a6a342ad973f132822b1d3b7789
ford-jones <fordnicholasjones@gmail.com>
2022-09-17 23:21:24 +1200

    Build skeleton

---
+++ client/components/Game.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+import Obstacle from './Obstacle'
+import Player from './Player'
+import PopUp from './Popup'
+
+export function Game() {
+  return (
+    <>
+      <table className="table">
+        <tr className="table">
+          <th className="table"></th>
+          <th className="table"></th>
+          <th className="table"></th>
+          <th className="table"></th>
+          <th className="table"></th>
+        </tr>
+        <tr className="table">
+          <td className="table"></td>
+          <td className="table"></td>
+          <td className="table"></td>
+          <td className="table"></td>
+          <td className="table"></td>
+        </tr>
+        <tr className="table">
+          <td className="table"></td>
+          <td className="table"></td>
+          <td className="table"></td>
+          <td className="table"></td>
+          <td className="table"></td>
+        </tr>
+        <tr className="table">
+          <td className="table"></td>
+          <td className="table"></td>
+          <td className="table"></td>
+          <td className="table"></td>
+          <td className="table"></td>
+        </tr>
+        <tr className="table">
+          <td className="table"></td>
+          <td className="table"></td>
+          <td className="table"></td>
+          <td className="table"></td>
+          <td className="table"></td>
+        </tr>
+      </table>
+    </>
+  )
+}
--- client/index.js
+++ client/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
-  render(<App />, document.getElementById('app'))
+  render(
+    <Router>
+      <App />
+    </Router>,
+    document.getElementById('app')
+  )
 })
