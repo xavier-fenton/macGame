e004f8bf87efba9cf9c8109ff26ed7e605b4e4ad
ford-jones <fordnicholasjones@gmail.com>
2022-09-18 17:37:54 +1200

    Change tab title, Access HTML collection of player td's

--- client/components/playerTrigger.js
+++ client/components/playerTrigger.js
@@ -1,13 +1,2 @@
-const playerCells = document.getElementsByClassName('player-cells')
-console.log('playerCells: ', playerCells)
-console.log('playerCells data type: ', typeof playerCells)
-
-// for (let i = 0; i < playerCells.length; i++) {
-//   const cell = playerCells[i]
-//   console.log('singular table cell: ', cell)
-// }
-
-// playerCells.map((cell) => {
-//   console.log(cell.body)
-//   return cell.body
-// })
+let cells = document.getElementsByClassName('player-cells')
+console.log(cells)
