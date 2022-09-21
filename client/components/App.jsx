33ba1e3f5c04263dc43df06d119abcc5ff7e0eed
Xavier Fenton <xavierfenton99@gmail.com>
2022-09-21 19:25:53 +1200

    can log cordinates

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,32 @@
-import React, { useEffect } from 'react'
-import { Game } from './Game'
+import React from 'react'
 
 export function App() {
+  const x = document.getElementById('demo')
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition)
+    } else {
+      x.innerHTML = 'Geolocation is not supported by this browser.'
+    }
+    console.log(navigator.geolocation)
+  }
+
+  function showPosition(position) {
+    // x.innerHTML =
+    //   'Latitude: ' +
+    //   position.coords.latitude +
+    //   '<br>Longitude: ' +
+    //   position.coords.longitude
+    console.log(position.coords.longitude, position.coords.latitude)
+  }
+
   return (
     <div>
-      <h1 className="header">Welcome to Mac Dancer!</h1>
-      <div>
-        <Game />
-      </div>
+      <h1>show cords</h1>
+      <button onClick={getLocation}>Try It</button>
+
+      <p id="demo"></p>
     </div>
   )
 }
